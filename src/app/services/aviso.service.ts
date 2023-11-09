46cce476caf7efdc624b2bbc605f51c8b8adf173
Cosme <118787355+cosme73@users.noreply.github.com>
2023-11-09 11:30:02 -0500

    Revert "Corrección de rutas con el backend"

--- src/app/services/aviso.service.ts
+++ src/app/services/aviso.service.ts
@@ -10,7 +10,7 @@ const base_url= environment.base;
   providedIn: 'root'
 })
 export class AvisoService {
-  private url = `${base_url}/avisos`;
+  private url = `${base_url}/aviso`;
   private listaCambio = new Subject<Aviso[]>();
   constructor(private http: HttpClient) {}
 
